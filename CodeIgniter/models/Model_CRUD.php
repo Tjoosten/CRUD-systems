934884dfe2bd2904e5c181ec960234802e3dde1c
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-01-04 08:19:54 +0100

    Update Model_CRUD.php

--- CodeIgniter/models/Model_CRUD.php
+++ CodeIgniter/models/Model_CRUD.php
@@ -1,18 +1,33 @@
 <?php 
   Class Model_CRUD extends CI_Model {
     public function Create() {
+      $Values = array(
+        'Employee' => $this->input-post('Employee'),
+        'Email'    => $this->input->post('Email'),
+        'Number'   => $this->input->post('Number'),
+      );
       
+      $this->db->insert('Employees');
     }
     
     public function Read() {
-      
+      $Query = $this->db->get('Employees');
+      return $Query->result();
     }
     
     public function Update() {
+      $Values = array(
+        'Employee' => $this->input-post('Employee'),
+        'Email'    => $this->input->post('Email'),
+        'Number'   => $this->input->post('Number'),
+      );
       
+      $this->db->where('Employee', $this->uri->segment(3))
+               ->update('Employees', $Values);
     }
     
     public function Delete() {
-      
+      $this->db->where('Employee', $this->uri->segment(3))
+               ->delete('Eùployees');
     }
   }
