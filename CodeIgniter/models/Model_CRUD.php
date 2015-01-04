5fc585bfd0454fb33d65176f8a531bf75923ea9c
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-01-04 08:12:58 +0100

    Update Model_CRUD.php

--- CodeIgniter/models/Model_CRUD.php
+++ CodeIgniter/models/Model_CRUD.php
@@ -1,4 +1,19 @@
 <?php 
   Class Model_CRUD extends CI_Model {
+    public function Create() {
+      
+    }
+    
+    public function Read() {
+      
+    }
+    
+    public function Update() {
+      
+    }
+    
+    public function Delete() {
+      
+    }
   }
 ?>
